973e0b40f4625cd6ff43f58198c51d382de392a7
Anbang Xu <anbangx@gmail.com>
2014-05-15 13:58:28 -0700

    Add seek function

--- cs143b/src/main/java/driver/FSDriver.java
+++ cs143b/src/main/java/driver/FSDriver.java
@@ -3,7 +3,6 @@ package driver;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Scanner;
@@ -40,18 +39,19 @@ public class FSDriver {
 		String file = input.nextLine();
 		File f = new File(file);
 		String absolutePath = f.getAbsolutePath();
-		String parent = absolutePath.substring(0,absolutePath.lastIndexOf(File.separator));
-		
+		String parent = absolutePath.substring(0,
+				absolutePath.lastIndexOf(File.separator));
+
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		File out = new File(parent + "/sid.txt");
 		FileWriter fw = new FileWriter(out.getAbsoluteFile());
 		BufferedWriter bw = new BufferedWriter(fw);
-		
+
 		// if file doesnt exists, then create it
 		if (!out.exists()) {
 			out.createNewFile();
 		}
-		
+
 		FileSystem fs = new FileSystem();
 		String line;
 		while ((line = br.readLine()) != null) {
@@ -66,9 +66,9 @@ public class FSDriver {
 
 	public static void executeInCommandLine() throws Exception {
 		Scanner input = new Scanner(System.in);
-		
+
 		FileSystem fs = new FileSystem();
-		
+
 		while (true) {
 			System.out.println("Shell> ");
 			String line = input.nextLine();
@@ -82,5 +82,5 @@ public class FSDriver {
 		}
 		input.close();
 	}
-	
+
 }
--- cs143b/src/main/java/fs/FileSystem.java
+++ cs143b/src/main/java/fs/FileSystem.java
@@ -1,7 +1,7 @@
 package fs;
 
 public class FileSystem {
-	
+
 	private static final int DIRECTORY_START_INDEX = 2;
 	private static final int DIRECTORY_END_INDEX = 4;
 	private static final int FILE_DESCRIPTOR_START_INDEX = 5;
@@ -9,265 +9,283 @@ public class FileSystem {
 	private static final int MAX_INDEX_WITHIN_BLOCK = 15;
 	private static final int SLOT_SIZE = 2;
 	private static final int FILE_DESCRIPTOR_SIZE = 4;
-	
+
 	private IOSystem io;
 	private OPTEntry[] OPT;
 	private long[] MASK;
-	
-	public FileSystem(){
+
+	public FileSystem() {
 		this.io = new IOSystem();
 		MASK = new long[64];
 		MASK[63] = 1;
-		for(int i = 62; i >= 0; i--){
-			MASK[i] = MASK[i+1] << 1;
+		for (int i = 62; i >= 0; i--) {
+			MASK[i] = MASK[i + 1] << 1;
 		}
 		OPT = new OPTEntry[4];
-		for(int i = 0; i < 4; i++){
+		for (int i = 0; i < 4; i++) {
 			OPT[i] = new OPTEntry();
 		}
 	}
-	
-	public String execute(String line){
+
+	public String execute(String line) {
 		String[] tokens = line.split(" ");
 		String command = tokens[0];
-		
-		if(command.equals("in")){
+
+		if (command.equals("in")) {
 			this.init();
 			return "disk initialized";
-		} else if(command.equals("cr")){
+		} else if (command.equals("cr")) {
 			String name = tokens[1];
 			this.create(name);
 			return name + " created";
-		} else if(command.equals("op")){
+		} else if (command.equals("op")) {
 			String name = tokens[1];
 			int OPTIdx = this.open(name);
 			return name + " opened " + OPTIdx;
-		} else if(command.equals("rd")){
+		} else if (command.equals("rd")) {
 			int OPTIdx = Integer.parseInt(tokens[1]);
 			int count = Integer.parseInt(tokens[2]);
 			String read = this.read(OPTIdx, count);
 			return read;
-		} else if(command.equals("wr")){
+		} else if (command.equals("wr")) {
 			int OPTIdx = Integer.parseInt(tokens[1]);
 			char c = tokens[2].charAt(0);
 			int count = Integer.parseInt(tokens[3]);
 			int num = this.write(OPTIdx, c, count);
-			return num + " bytes written"; 
-		} else if(command.equals("lsdisk")){
+			return num + " bytes written";
+		} else if (command.equals("lsdisk")) {
 			return io.toString();
 		}
 		return "";
 	}
-	
-	public void init(){
+
+	public void init() {
 		// 1. initiate bitmap
 		setBitMap(0);
-		setBitMap(1);	// file descriptor for directory
-		setBitMap(2);	// directory
-		setBitMap(3);	// directory
-		setBitMap(4);	// directory
-		
+		setBitMap(1); // file descriptor for directory
+		setBitMap(2); // directory
+		setBitMap(3); // directory
+		setBitMap(4); // directory
+
 		// 2. initiate the directory
 		initDirectory();
 	}
-	
-	public void initDirectory(){
+
+	public void initDirectory() {
 		int[] block = io.readBlock(1);
 		block[0] = 0; // length of files
 		block[1] = 2;
 		block[2] = 3;
 		block[3] = 4;
 	}
-	
-	public void create(String name){
+
+	public void create(String name) {
 		// 1. find a free file descriptor
 		int curBlockIdx = FILE_DESCRIPTOR_START_INDEX;
 		int curReference = 0;
 		int[] fdBlock = io.readBlock(curBlockIdx);
-		while(curBlockIdx <= FILE_DESCRIPTOR_END_INDEX){
-			if(fdBlock[curReference] < 0){ // find free
+		while (curBlockIdx <= FILE_DESCRIPTOR_END_INDEX) {
+			if (fdBlock[curReference] < 0) { // find free
 				// update length
 				fdBlock[curReference] = 0;
 				break;
 			}
 			curReference += FILE_DESCRIPTOR_SIZE;
-			if(curReference > MAX_INDEX_WITHIN_BLOCK){
+			if (curReference > MAX_INDEX_WITHIN_BLOCK) {
 				// check next file descriptor block
 				curBlockIdx++;
 				fdBlock = io.readBlock(curBlockIdx);
 				curReference = 0;
 			}
 		}
-		
+
 		// 2. find a free directory entry
 		int curDirIdx = DIRECTORY_START_INDEX;
-		int curSlotIdx = 0; // 1st index {name, index} 
+		int curSlotIdx = 0; // 1st index {name, index}
 		int[] dirBlock = io.readBlock(curDirIdx);
-		while(curDirIdx <= DIRECTORY_END_INDEX){
-			if(dirBlock[curSlotIdx + 1] < 0){ // find free
+		while (curDirIdx <= DIRECTORY_END_INDEX) {
+			if (dirBlock[curSlotIdx + 1] < 0) { // find free
 				// update name and index
 				dirBlock[curSlotIdx] = convertStringToInt(name);
-				dirBlock[curSlotIdx + 1] = (curBlockIdx - FILE_DESCRIPTOR_START_INDEX) * 4 + curReference / 4;
+				dirBlock[curSlotIdx + 1] = (curBlockIdx - FILE_DESCRIPTOR_START_INDEX)
+						* 4 + curReference / 4;
 				break;
 			}
 			curSlotIdx += SLOT_SIZE;
-			if(curSlotIdx > MAX_INDEX_WITHIN_BLOCK){
+			if (curSlotIdx > MAX_INDEX_WITHIN_BLOCK) {
 				// check next directory block
 				curDirIdx++;
 				dirBlock = io.readBlock(curSlotIdx);
 				curSlotIdx = 0;
 			}
 		}
-		
+
 		// 3. write back the updates to disk
 		io.writeBlock(curBlockIdx, fdBlock);
 		io.writeBlock(curDirIdx, dirBlock);
 	}
-	
-	public void destroy(String name){
+
+	public void destroy(String name) {
 		// 1. search directory to find file descriptor
 		int curDirIdx = DIRECTORY_START_INDEX;
-		int curSlotIdx = 0; // 1st index {name, index} 
+		int curSlotIdx = 0; // 1st index {name, index}
 		int[] dirBlock = io.readBlock(curDirIdx);
 		int nameToInt = convertStringToInt(name);
 		int fdIdx = -1;
 		int[] fdBlock = null;
-		while(curDirIdx < DIRECTORY_END_INDEX){
-			if(dirBlock[curSlotIdx] == nameToInt){ // find!
+		while (curDirIdx < DIRECTORY_END_INDEX) {
+			if (dirBlock[curSlotIdx] == nameToInt) { // find!
 				// 2. free file descriptor
 				fdIdx = curSlotIdx / 4 + 5;
 				int fdReference = curSlotIdx % 4;
 				fdBlock = io.readBlock(fdIdx);
 				fdBlock[fdReference] = -1;
 				// 3. update bitmap
-				for(int i = 1; i < 4; i++){
+				for (int i = 1; i < 4; i++) {
 					setBitMap(fdBlock[fdReference + i]);
 				}
 				// 4. remove directory entry
 				dirBlock[curSlotIdx + 1] = -1;
 			}
 			curSlotIdx += SLOT_SIZE;
-			if(curSlotIdx > MAX_INDEX_WITHIN_BLOCK){
+			if (curSlotIdx > MAX_INDEX_WITHIN_BLOCK) {
 				// check next directory block
 				curDirIdx++;
 				dirBlock = io.readBlock(curSlotIdx);
 				curSlotIdx = 0;
 			}
 		}
-		
+
 		// 5. write back the updates to disk
 		io.writeBlock(fdIdx, fdBlock);
 		io.writeBlock(curDirIdx, dirBlock);
 	}
-	
-	public int open(String name){
+
+	public int open(String name) {
 		// 1. search directory to find file descriptor
 		int slotIdx = getSlotIdx(name);
-		if(slotIdx < 0){
+		if (slotIdx < 0) {
 			System.out.println(name + " doesn't exist!");
 			return -1;
 		}
-		
+
 		// 2. allocate a free OPT entry
 		int freeOPTIdx = getFreeOPTEntryIdx();
-		
+
 		// 3 fill in current position and file descriptor index
 		OPT[freeOPTIdx].currentPosition = 0;
 		OPT[freeOPTIdx].index = slotIdx;
-		
+
 		// 4 search first data block - update bitmap, update file descriptor
 		int[] fdBlock = getFDBlockFromSlotIdx(slotIdx);
-		if(fdBlock[slotIdx * 4 + 1] == -1){
+		if (fdBlock[slotIdx * 4 + 1] == -1) {
 			int newBlockIdx = searchAndUpdateBitMap();
 			fdBlock[slotIdx * 4 + 1] = newBlockIdx;
 		}
-		
+
 		// 5. read block 0 of file into the r/w buffer(read-ahead)
 		int fileLength = fdBlock[slotIdx * 4];
 		int firstDataBlockIdx = fdBlock[slotIdx * 4 + 1];
 		OPT[freeOPTIdx].length = fileLength;
-		if(firstDataBlockIdx != -1)
+		if (firstDataBlockIdx != -1)
 			OPT[freeOPTIdx].buffer = io.readBlock(firstDataBlockIdx);
-		
+
 		return freeOPTIdx;
 	}
-	
-	public boolean close(int OPTIdx){
+
+	public boolean close(int OPTIdx) {
 		// 1. write buffer to disk
-		int dataBlockIdx = getDataBlockIdxFromOPTEntry(OPT[OPTIdx]);
+		int dataBlockIdx = getDataBlockIdxFromOPTEntry(OPT[OPTIdx], 1);
 		io.writeBlock(dataBlockIdx, OPT[OPTIdx].buffer);
-		
+
 		// 2. update file length in descriptor
 		int slotIdx = OPT[OPTIdx].index;
 		int[] fdBlock = getFDBlockFromSlotIdx(slotIdx);
 		fdBlock[slotIdx % 4] = OPT[OPTIdx].length;
-		
+
 		// 3. free OPT entry
 		OPT[OPTIdx].index = -1;
-		  
+
 		// 4. return status
 		return true;
 	}
-	
-	public String read(int OPTIdx, int count){
+
+	public String read(int OPTIdx, int count) {
 		// 1. compute position in the r/w buffer
 		int currentPosition = OPT[OPTIdx].currentPosition;
 		int length = OPT[OPTIdx].length;
 		StringBuilder sb = new StringBuilder();
-		
-		if(currentPosition + count < length){ // not reach the end of buffer
+
+		if (currentPosition + count < length) { // not reach the end of buffer
 			// 2. read buffer
-			for(int i = 0; i < count; i++){
+			for (int i = 0; i < count; i++) {
 				char c = OPT[OPTIdx].readCharFromBuffer(currentPosition + i);
 				sb.append(c);
 			}
 		}
 		return sb.toString();
 	}
-	
-	public int write(int OPTIdx, char c, int count){
+
+	public int write(int OPTIdx, char c, int count) {
 		// 1. compute position in the r/w buffer
 		int currentPosition = OPT[OPTIdx].currentPosition;
-		int fdIdx = OPT[OPTIdx].index;
-		
-		if(currentPosition + count <= 64){ // not reach the end of buffer
+//		int fdIdx = OPT[OPTIdx].index;
+
+		if (currentPosition + count <= 64) { // not reach the end of buffer
 			// if block doesn't exist
-//			if( == -1){
-//				// allocate new block
-//				int newBlockIdx = searchAndUpdateBitMap();
-//				OPT[OPTIdx].index = newBlockIdx;
-//				OPT[OPTIdx].buffer = io.readBlock(newBlockIdx);
-//			}
+			// if( == -1){
+			// // allocate new block
+			// int newBlockIdx = searchAndUpdateBitMap();
+			// OPT[OPTIdx].index = newBlockIdx;
+			// OPT[OPTIdx].buffer = io.readBlock(newBlockIdx);
+			// }
 			// 2. write text to buffer
-			for(int i = 0; i < count; i++){
+			for (int i = 0; i < count; i++) {
 				OPT[OPTIdx].writeCharToBuffer(c, currentPosition + i);
 				OPT[OPTIdx].currentPosition++;
 				OPT[OPTIdx].length++;
 			}
-			
-//			// 3. write the buffer to disk block
-//			int dataBlockIdx = getDataBlockIdxFromOPTEntry(OPT[OPTIdx]);
-//			io.writeBlock(dataBlockIdx, OPT[OPTIdx].buffer);
-			
-//			// 4. update file length in descriptor
-//			int slotIdx = OPT[OPTIdx].index;
-//			int[] fdBlock = getFDBlockFromSlotIdx(slotIdx);
-//			fdBlock[0] = OPT[OPTIdx].length;
+
+			// // 3. write the buffer to disk block
+			// int dataBlockIdx = getDataBlockIdxFromOPTEntry(OPT[OPTIdx]);
+			// io.writeBlock(dataBlockIdx, OPT[OPTIdx].buffer);
+
+			// // 4. update file length in descriptor
+			// int slotIdx = OPT[OPTIdx].index;
+			// int[] fdBlock = getFDBlockFromSlotIdx(slotIdx);
+			// fdBlock[0] = OPT[OPTIdx].length;
 		}
 		return count;
 	}
 	
-	public int getSlotIdx(String name){
+	public void seek(int OPTIdx, int target){
+		int curDataBlockNum = OPT[OPTIdx].currentPosition % 64;
+		int targetDataBlockNum = target % 64;
+		if(curDataBlockNum != targetDataBlockNum){ // if the new position is not within the current block
+			int slotIdx = OPT[OPTIdx].index;
+			// 1. write the old buffer to disk
+			int[] fdBlock = getFDBlockFromSlotIdx(slotIdx);
+			int oldDataBlockIdx = fdBlock[slotIdx % 4 + curDataBlockNum];
+			io.writeBlock(oldDataBlockIdx, OPT[OPTIdx].buffer);
+			
+			// 2. read the new block to OPT
+			int newDataBlockIdx = fdBlock[slotIdx % 4 + targetDataBlockNum];
+			OPT[OPTIdx].buffer = io.readBlock(newDataBlockIdx);
+		}
+		OPT[OPTIdx].currentPosition = target;
+	}
+	
+	public int getSlotIdx(String name) {
 		int curDirIdx = DIRECTORY_START_INDEX;
-		int curSlotIdx = 0; // 1st index {name, index} 
+		int curSlotIdx = 0; // 1st index {name, index}
 		int[] dirBlock = io.readBlock(curDirIdx);
 		int nameToInt = convertStringToInt(name);
-		while(curDirIdx <= DIRECTORY_END_INDEX){
-			if(dirBlock[curSlotIdx] == nameToInt){ // find!
+		while (curDirIdx <= DIRECTORY_END_INDEX) {
+			if (dirBlock[curSlotIdx] == nameToInt) { // find!
 				return curSlotIdx;
 			}
 			curSlotIdx += SLOT_SIZE;
-			if(curSlotIdx > MAX_INDEX_WITHIN_BLOCK){
+			if (curSlotIdx > MAX_INDEX_WITHIN_BLOCK) {
 				// check next directory block
 				curDirIdx++;
 				dirBlock = io.readBlock(curSlotIdx);
@@ -276,20 +294,20 @@ public class FileSystem {
 		}
 		return -1;
 	}
-	
-	public int getFreeOPTEntryIdx(){
-		for(int i = 0; i < OPT.length; i++){
-			if(OPT[i].index < 0){
+
+	public int getFreeOPTEntryIdx() {
+		for (int i = 0; i < OPT.length; i++) {
+			if (OPT[i].index < 0) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	
-	public int searchAndUpdateBitMap(){
+
+	public int searchAndUpdateBitMap() {
 		long bitmap = getBitMap();
-		for(int i = 0; i < 64; i++){
-			if((bitmap & MASK[i]) == 0){
+		for (int i = 0; i < 64; i++) {
+			if ((bitmap & MASK[i]) == 0) {
 				// find! stop search
 				setBitMap(i);
 				return i;
@@ -297,81 +315,79 @@ public class FileSystem {
 		}
 		return -1;
 	}
-	
-	public int getDataBlockIdxFromOPTEntry(OPTEntry entry){
+
+	public int getDataBlockIdxFromOPTEntry(OPTEntry entry, int whickBlock) {
 		int slotIdx = entry.index;
 		int[] fdBlock = getFDBlockFromSlotIdx(slotIdx);
-		int firstDataBlockIdx = fdBlock[slotIdx % 4 + 1];
+		int firstDataBlockIdx = fdBlock[slotIdx % 4 + whickBlock];
 		return firstDataBlockIdx;
 	}
-	
-	public int[] getFDBlockFromSlotIdx(int curSlotIdx){
+
+	public int[] getFDBlockFromSlotIdx(int curSlotIdx) {
 		int fdIdx = curSlotIdx / 4 + 5;
 		return io.readBlock(fdIdx);
 	}
-	
-	public int getReferenceFromSlotIdx(int curSlotIdx){
+
+	public int getReferenceFromSlotIdx(int curSlotIdx) {
 		return curSlotIdx % 4;
 	}
-	
-	public long getBitMap(){
+
+	public long getBitMap() {
 		int[] block = io.readBlock(0);
 		return convertToLong(block[0], block[1]);
 	}
-	
-	public void setBitMap(int i){
-		if(i == -1)
+
+	public void setBitMap(int i) {
+		if (i == -1)
 			return;
 		long bitMap = getBitMap();
 		bitMap = bitMap | MASK[i];
-		int[] block = new int[16]; 
-		block[0] = (int)(bitMap >> 32);
-		block[1] = (int)bitMap;
+		int[] block = new int[16];
+		block[0] = (int) (bitMap >> 32);
+		block[1] = (int) bitMap;
 		io.writeBlock(0, block);
 	}
-	
-	public long convertToLong(int a, int b){
-		long c = (long)a << 32 | b & 0xFFFFFFFFL;
+
+	public long convertToLong(int a, int b) {
+		long c = (long) a << 32 | b & 0xFFFFFFFFL;
 		return c;
 	}
-	
-	public int convertStringToInt(String name){
+
+	public int convertStringToInt(String name) {
 		StringBuilder s = new StringBuilder(name);
 		s.setLength(4);
 		byte[] buffer = s.toString().getBytes();
 		return byteArrayToInt(buffer);
 	}
-	
-	public String convertIntToString(int a){
-		byte[] bytes= intToByteArray(a);
+
+	public String convertIntToString(int a) {
+		byte[] bytes = intToByteArray(a);
 		return new String(bytes);
 	}
-	
-	public int byteArrayToInt(byte[] b) 
-	{
-	    int value = 0;
-	    for (int i = 0; i < 4; i++) {
-	        int shift = (4 - 1 - i) * 8;
-	        value += (b[i] & 0x000000FF) << shift;
-	    }
-	    return value;
+
+	public int byteArrayToInt(byte[] b) {
+		int value = 0;
+		for (int i = 0; i < 4; i++) {
+			int shift = (4 - 1 - i) * 8;
+			value += (b[i] & 0x000000FF) << shift;
+		}
+		return value;
 	}
 
-	public byte[] intToByteArray(int a)
-	{
-	    byte[] ret = new byte[4];
-	    ret[3] = (byte) (a & 0xFF);   
-	    ret[2] = (byte) ((a >> 8) & 0xFF);   
-	    ret[1] = (byte) ((a >> 16) & 0xFF);   
-	    ret[0] = (byte) ((a >> 24) & 0xFF);
-	    return ret;
+	public byte[] intToByteArray(int a) {
+		byte[] ret = new byte[4];
+		ret[3] = (byte) (a & 0xFF);
+		ret[2] = (byte) ((a >> 8) & 0xFF);
+		ret[1] = (byte) ((a >> 16) & 0xFF);
+		ret[0] = (byte) ((a >> 24) & 0xFF);
+		return ret;
 	}
-	
-	public int[] convertStringToIntArray(String s){
+
+	public int[] convertStringToIntArray(String s) {
 		int len = s.length();
 		int num = len / 4;
 		int[] results = new int[num];
-		for(int i = 0; i < num; i++){
+		for (int i = 0; i < num; i++) {
 			results[i] = convertStringToInt(s.substring(4 * i, 4));
 		}
 		return results;
