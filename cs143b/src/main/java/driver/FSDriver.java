54d1721ed80c128cb1ef8d3b2e2ac9e7a54b3fd2
Anbang Xu <anbangx@gmail.com>
2014-05-15 11:31:48 -0700

    Add option to read from file

--- cs143b/src/main/java/driver/FSDriver.java
+++ cs143b/src/main/java/driver/FSDriver.java
@@ -1,5 +1,11 @@
 package driver;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 import fs.FileSystem;
@@ -28,8 +34,34 @@ public class FSDriver {
 		input.close();
 	}
 
-	public static void executeFromInputFile() {
-
+	public static void executeFromInputFile() throws Exception {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Provide the input path: ");
+		String file = input.nextLine();
+		File f = new File(file);
+		String absolutePath = f.getAbsolutePath();
+		String parent = absolutePath.substring(0,absolutePath.lastIndexOf(File.separator));
+		
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		File out = new File(parent + "/sid.txt");
+		FileWriter fw = new FileWriter(out.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		// if file doesnt exists, then create it
+		if (!out.exists()) {
+			out.createNewFile();
+		}
+		
+		FileSystem fs = new FileSystem();
+		String line;
+		while ((line = br.readLine()) != null) {
+			System.out.println("---------------");
+			// process the line.
+			bw.write(fs.execute(line) + "\n");
+		}
+		br.close();
+		input.close();
+		bw.close();
 	}
 
 	public static void executeInCommandLine() throws Exception {
